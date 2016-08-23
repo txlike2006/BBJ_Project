6575e70047519992cad97864c71bf099e5cb2768
txlike <422389731@qq.com>
2016-08-23 15:44:42 +0800

    [dev] add image model

---
+++ BBJ_Project/Classes/Modules/Main/Entity/DetailImageWebModel.h
@@ -0,0 +1,17 @@
+//
+//  DetailImageWebModel.h
+//  BBJ_Project
+//
+//  Created by BJB041511-032 on 16/8/23.
+//  Copyright © 2016年 BJB041511-032. All rights reserved.
+//
+
+#import "HVEntity.h"
+
+@interface DetailImageWebModel : HVEntity
+
+@property (nonatomic ,copy) NSString *src;
+@property (nonatomic ,copy) NSString *pixel;
+@property (nonatomic ,copy) NSString *ref;
+
+@end
---
+++ BBJ_Project/Classes/Modules/Main/Entity/DetailImageWebModel.m
@@ -0,0 +1,13 @@
+//
+//  DetailImageWebModel.m
+//  BBJ_Project
+//
+//  Created by BJB041511-032 on 16/8/23.
+//  Copyright © 2016年 BJB041511-032. All rights reserved.
+//
+
+#import "DetailImageWebModel.h"
+
+@implementation DetailImageWebModel
+
+@end
